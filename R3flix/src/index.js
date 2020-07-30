1c7b7b1b8935d5c04da84ddcd463ff80b8f45fab
Ricardo Rocha Ribeiro <r3ifes@gmail.com>
2020-07-29 21:49:47 -0300

    Commit do segundo dia a pedidos do Marco

--- R3flix/src/index.js
+++ R3flix/src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+
+
+//Desafio da criação de um jogo para essa página
+const Page404 = () => (<h1>Página 404</h1>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+  <Switch>
+    <Route path="/" component={Home} exact/>
+    <Route path="/cadastro/video" component={CadastroVideo} />
+    <Route path="/cadastro/categoria" component={CadastroCategoria} />
+    <Route component={Page404} />
+  </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
