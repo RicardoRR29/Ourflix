b1adb110e88e5296812733f66ea9810845e369bd
Ricardo Rocha Ribeiro <r3ifes@gmail.com>
2020-07-30 04:52:16 -0300

    Aula 2 finalizada

--- R3flix/src/components/Footer/styles.js
+++ R3flix/src/components/Footer/styles.js
@@ -2,15 +2,16 @@ import styled from 'styled-components';
 
 
 export const FooterBase = styled.footer`
+  
   background: var(--black);
   border-top: 2px solid var(--primary);
   padding-left: 16px;
   padding-right: 16px;
-  padding-top: 32px;
-  padding-bottom: 32px;
+  padding-top: 20px;
+  padding-bottom: 5px;
   color: var(--white);
   text-align: center;
   @media (max-width: 800px) {
-    margin-bottom: 50px;
+    margin-bottom: 10px;
   }
 `;
